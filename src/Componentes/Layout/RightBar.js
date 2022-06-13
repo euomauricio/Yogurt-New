9a66d817681b319139cc35fd3e262e6b01489677
Vanessa <vanessa.lopes@alunos.sc.senac.br>
2022-06-13 15:52:27 -0300

    Alterações RightBar e LeftBar do Feed.

--- src/Componentes/Layout/RightBar.js
+++ src/Componentes/Layout/RightBar.js
@@ -1,7 +1,13 @@
-import dog from '../../Assets/dog.png';
-import skate from '../../Assets/skate.png';
-import livro from '../../Assets/livro.png';
+import usuario1 from '../../Assets/usuario1.png';
+import usuario2 from '../../Assets/usuario2.png';
+import usuario3 from '../../Assets/usuario3.png';
+import usuario4 from '../../Assets/usuario4.png';
+import usuario5 from '../../Assets/usuario5.png';
+import usuario6 from '../../Assets/usuario6.png';
+import usuario7 from '../../Assets/usuario7.png';
+import usuario8 from '../../Assets/usuario8.png';
 import disponivel from '../../Assets/disponivel.png';
+
 
 export default function RightBar() {
     return (
@@ -9,42 +15,79 @@ export default function RightBar() {
            <h3>Amigos</h3>
             <div>
             
-                <div className="comunidades">
-                <img className="dog" src={dog}></img><h5>Cristina Priester</h5>
+            <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario1}></img> 
+                    </div>
+                    <h5>Cristina Priester</h5>
                 </div>
 
                 <div className="amizades">
                     <img  className="disponivel" src={disponivel}></img>
                     
                     <div className="amigos">                
-                    <img src={skate}></img> <h5>Mauricio</h5>
+                    <img src={usuario2}></img> 
                     </div>
+                    <h5>Mauricio Luiz</h5>
                 </div>
+
+                <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario8}></img> 
+                    </div>
+                    <h5>Vanessa Lopes</h5>
+                </div>
+
+                 <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario3}></img> 
+                    </div>
+                    <h5>João Marcos</h5>
+                </div>
+
+                <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario4}></img> 
+                    </div>
+                    <h5>Alexandre Hausmann</h5>
+                </div>
+
+                <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario5}></img> 
+                    </div>
+                    <h5>Matheus Petri</h5>
+                </div>
+
+                <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario6}></img> 
+                    </div>
+                    <h5>Leandro Schmitt Elias</h5>
+                </div>
+
+                <div className="amizades">
+                    <img  className="disponivel" src={disponivel}></img>
+                    
+                    <div className="amigos">                
+                    <img src={usuario7}></img> 
+                    </div>
+                    <h5>Rodrigo Leonardo da Silva</h5>
+                </div>
+
                 
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>João</h5>
-                </div>
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>Alexandre</h5>
-                </div>
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>Matheus</h5>
-                </div>
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>Leandro</h5>
-                </div>
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>Rodrigo</h5>
-                </div>
-
-                <div className="comunidades">
-                <img classname="dog" src={livro}></img><h5>Vanessa Lopes</h5>
-                </div>
                 
             </div> 
         </div>
